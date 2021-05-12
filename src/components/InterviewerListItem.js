b58622f9a20a7664e4043ec3935339b1343fc834
Michelle <michellelverge@gmail.com>
2021-05-12 07:19:59 -0700

    refactors InterviewerListItem to use values from props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,18 +1,25 @@
 import React from "react";
 
 import "components/InterviewerListItem.scss";
+import classNames from 'classnames/bind';
 
 
 export default function InterviewerListItem(props) {
 
+  let interviewerClass = classNames ( {
+    "interviewers__item": true,
+    "interviewers__item--selected": props.selected,
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass}
+    onClick={() => props.setInterviewer(props.name)}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   )
 }
