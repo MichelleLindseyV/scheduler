35cd5e2b4fc06037e3caf20c2339ea4e35fe0b6c
Michelle <michellelverge@gmail.com>
2021-05-12 15:49:10 -0700

    refactors interviewer list to use prop values

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,14 +1,28 @@
 import React from "react";
 
+
 import "components/InterviewerList.scss";
+import classNames from 'classnames/bind';
 import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
-  
+  console.log(props)
+  const interviewerData = props.interviewers.map(interviewers => {
+    return (
+      <InterviewerListItem
+        id={interviewers.id}
+        name={interviewers.name}
+        avatar={interviewers.avatar}
+        selected={interviewers.id === props.interviewer}
+       />
+    );
+  }) 
   return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
-    </section>
+  <section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list">
+    {interviewerData}
+  </ul>
+  </section>
   );
 }
