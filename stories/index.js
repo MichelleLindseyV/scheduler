2da2c410719cc96637f3619444901540dac834c2
Michelle <michellelverge@gmail.com>
2021-05-11 17:44:21 -0700

    creates and populates DayList component and associated story

--- stories/index.js
+++ stories/index.js
@@ -7,9 +7,10 @@ import "index.scss";
 
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
 
 
-//Button Stories:
+//Button Story:
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -27,7 +28,7 @@ storiesOf("Button", module)
   ));
 
 
-  //DayListItem Stories:
+  //DayListItem Story:
   storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -38,3 +39,34 @@ storiesOf("Button", module)
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
+
+
+  //DayList Story:
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  
+  storiesOf("DayList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Monday", () => (
+      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    ))
+    .add("Tuesday", () => (
+      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    ));
