7432ebff9808fd6cd628f6efafac50576be6673d
Michelle <michellelverge@gmail.com>
2021-05-12 18:11:05 -0700

    creates confirm component and stories

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 //Button Story:
@@ -158,5 +159,12 @@ storiesOf("Button", module)
           interviewer="Sylvia Palmer"
           onEdit={action("onEdit")}
           onDelete={action("onDelete")}
+          />
+        ))
+        .add("Confirm", () => (
+          <Confirm 
+          message="Delete the appoinment?"
+          onConfirm={action("onConfirm")}
+          onCancel={action("onCancel")}
           />
         ))
