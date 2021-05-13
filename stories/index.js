299a88718bc0625e54c1703342d3d148b05afa16
Michelle <michellelverge@gmail.com>
2021-05-12 18:22:09 -0700

    creates status component and stories

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 
 //Button Story:
@@ -168,3 +169,4 @@ storiesOf("Button", module)
           onCancel={action("onCancel")}
           />
         ))
+        .add("Status", () => <Status message="Deleting"/>)
