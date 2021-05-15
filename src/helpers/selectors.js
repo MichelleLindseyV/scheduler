d04e697a05cb1fe7ca4616cec148e9faea21f621
Michelle <michellelverge@gmail.com>
2021-05-15 15:09:01 -0700

    selector function retrieves appointments object data by selected day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,22 @@
+
+
+export function getAppointmentsForDay(state, day) {
+  
+  let appointmentArr = [];
+  let daysAppointments = [];
+  for (let i = 0; i < state.days.length; i++) {
+    if (state.days[i].name === day) {
+      appointmentArr = state.days[i].appointments
+    }
+  }
+  for (let j = 0; j < appointmentArr.length; j++) {
+    for (const prop in state.appointments) {
+      let appointmentId = state.appointments[prop].id
+    
+    if (appointmentArr[j] === appointmentId) {
+      daysAppointments.push(state.appointments[prop])
+    }
+  }
+} 
+  return daysAppointments;
+}
