ab6599777a2d34f016a9198dcc38f6b451ec6d4a
Michelle <michellelverge@gmail.com>
2021-05-19 13:34:03 -0700

    updates new appointment state locally

--- src/components/Application.js
+++ src/components/Application.js
@@ -24,10 +24,27 @@ export default function Application(props) {
   
   const setDay = day => setState({ ...state, day });
 
+
   function bookInterview(id, interview) {
     console.log(id, interview);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview}
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    
+    setState({
+      ...state,
+      appointments
+    });
   };
 
+  
 
   useEffect(() => {
     Promise.all([
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -14,19 +14,18 @@ const CREATE = "CREATE";
 
 export default function Appointment(props) {
 
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   function save(name, interviewer) {
     const interview = {
       student: name,
       interviewer
     };
-    props.bookInterview(props.id, interview)
+    props.bookInterview(props.id, interview);
+    transition(SHOW);
   }
-
-  const { mode, transition, back } = useVisualMode(
-    props.interview ? SHOW : EMPTY
-  );
-  
-
   
 
   return (
